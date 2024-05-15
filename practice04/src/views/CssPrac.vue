457cb8cc7947183bbca6ae030d4759fe7329b8cf
JerryPark <dev@teamflyers.kr>
2024-05-15 19:04:06 +0900

    Update CssPrac.vue

--- practice04/src/views/CssPrac.vue
+++ practice04/src/views/CssPrac.vue
@@ -1,56 +1,73 @@
 <template>
-    <div class="card">
+    <button @click="showCard">toggle</button>
+    <div class="card" v-if="isShown">
         <label>REPORTS</label>
         <ul>
-            <li><i class="fa-solid fa-clock"></i>Real-Time</li>
-            <li><i class="fas fa-user"></i>Audience</li>
-            <li><i class="fa-solid fa-flag"></i>Conversions</li>
+            <li>
+                <span class="icon"><i class="fa-solid fa-clock"></i></span>
+                Real-Time
+            </li>
+            <li>
+                <span class="icon"><i class="fas fa-user"></i></span>
+                Audience
+            </li>
+            <li>
+                <span class="icon"><i class="fa-solid fa-flag"></i></span>
+                    Conversions
+                </li>
         </ul>
     </div>
 </template>
 <script setup>
+import {ref} from 'vue'
+
+const isShown = ref(false);
+
+const showCard = () => isShown.value = !isShown.value;
 
 </script>
-<style scoped>
-.card{
-    border-style: solid;
-    border-width: 1px;
-    border-radius: 5px;
-    border-color: gainsboro;
-    padding: 20px;
+<style>
+.card {
+    width: 300px;
+    padding: 15px 0 10px;
     margin: auto;
-    width:300px;
+    border: 1px solid gainsboro;
+    border-radius: 5px;
     box-shadow: 0 2px 2px rgba(0, 0, 0, 0.3);
-    align-items: center;
 }
 
-h4{
-    margin-bottom: 15px;
-    color:#444444;
-}
-
-label {
+.card > label {
     display: block; /* label을 블록 레벨 요소로 만들어 margin 적용 가능하게 함 */
-    margin-bottom: 15px;
+    margin-bottom: 10px;
+    padding: 0 20px;
     color: gray;
     font-weight: 400;
     text-align: left;
 }
 
-i{
-    padding-right: 20px;
-    color:gray;
-}
-
-ul {
+.card > ul {
     padding-left: 0; /* 왼쪽 패딩 제거 */
     list-style-type: none; /* 리스트 스타일 제거 */
     text-align: left;
 }
 
-li {
-    margin-bottom: 0.5rem; /* 각 항목 사이의 간격 조정 */
+.card > ul > li {
+    padding: 0.25rem 20px; /* 각 항목 사이의 간격 조정 */
     font-weight: 500;
+    cursor: pointer;
+}
+
+.card > ul > li:hover {
+    background-color: #f0f0f0;
+}
+
+.card > ul > li > .icon {
+    display: inline-block;
+    width: 34px;
+}
+
+.card > ul > li > .icon > i{
+    color:gray;
 }
 
 </style>
